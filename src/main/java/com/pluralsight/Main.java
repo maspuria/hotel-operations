819b823d946b4046886a42a467797f37168ec3f1
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-20 15:05:55 -0500

    testing the reservation total

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,12 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+    // Testing the reservation total - not on the weekend
+        Reservation reservation1 = new Reservation("King", 1,false);
+        System.out.println(reservation1.getReservationTotal());
+    // Testing the reservation total - on the weekend
+        Reservation reservation2 = new Reservation("King", 1, true);
+        System.out.println(reservation2.getReservationTotal());
 
     }
 
