172c7b976c0ff0070573c4b19f656670ba31d49a
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-20 15:28:43 -0500

    employee class field members and getters commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,6 +1,35 @@
 package com.pluralsight;
 
 public class Employee {
+    // field members (backing variables)
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    //Constructors
+
+    //Getters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
 
 
 }
