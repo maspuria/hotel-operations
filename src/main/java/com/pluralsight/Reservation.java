f04cb878ec637e5fc089bb19e7e3ea5651eca0ae
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-20 14:04:38 -0500

    create reservation field members, constructor, getters commit

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
 public class Reservation {
+    // field members or attributes
+    private String roomType;
+    private int numberOfNights;
+    private boolean IsWeekend;
+    private int price;
+
+    // constructor
+
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend, int price) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        IsWeekend = isWeekend;
+        this.price = price;
+    }
+
+    //getters and setters
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return IsWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        IsWeekend = weekend;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    // derived getter
+    public double getReservationTotal(){
+      return
+    }
+
 }
