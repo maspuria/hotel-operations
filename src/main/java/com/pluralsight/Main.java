f1017f905789517fe08903f7be4d65e702cc6acd
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-20 16:32:34 -0500

    testing regular hours worked in main application

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,9 @@ public class Main {
     // Testing the reservation total - on the weekend
         Reservation reservation2 = new Reservation("King", 1, true);
         System.out.println(reservation2.getReservationTotal());
-
+    // Testing the employee total pay worked
+        Employee employee1 = new Employee(8888,"Mari", "Management",33.75,11);
+        System.out.println("Total Pay: $" + employee1.getTotalPay() + " For " + employee1.getName());
     }
 
 }
